f4a891ab9f5e7624efb6131ed4f5a245f008140d
esdmr <esdmr0@gmail.com>
2021-08-06 23:30:41 +0430

    Add api-extractor

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,11 @@
+/**
+ * "Greetings are nice things to say when people meet each other."
+ * {@link https://simple.wikipedia.org/wiki/Greeting}
+ *
+ * @public
+ * @param whom - Whom to greet
+ * @returns
+ */
 export default function greet (whom = 'World') {
 	return `Hello, ${whom}!`;
 }

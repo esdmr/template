2046a11e8b0afaef0baf30076ae6e4c93ca3f002
esdmr <esdmr0@gmail.com>
2021-08-06 16:12:44 +0430

    Rename tap test variable

--- test/main.ts
+++ test/main.ts
@@ -1,12 +1,12 @@
-import * as _ from 'tap';
+import { test } from 'tap';
 import greet from '#src/main.js';
 
-await _.test('greet', async (_) => {
-	_.equal(greet(), 'Hello, World!',
+await test('greet', async (t) => {
+	t.equal(greet(), 'Hello, World!',
 		'expected to greet "world" if not given a parameter');
 
-	_.equal(greet('Steve'), 'Hello, Steve!',
+	t.equal(greet('Steve'), 'Hello, Steve!',
 		'expected to greet if given a parameter');
 
-	_.end();
+	t.end();
 });

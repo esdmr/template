f8d7ed12b3209aed26f45ec7025690693d832b78
esdmr <esdmr0@gmail.com>
2021-11-04 21:53:50 +0330

    Disable node-tap coverage

--- scripts/coverage-map.cjs
+++
@@ -1,2 +0,0 @@
-const regex = /^build\/test\/(?<dir>(?:[^/]+(?<!\.d)\/)*)(?:(?<dpath>[^/]+?)\.d.*|(?<fpath>[^/]+)\.js)$/u;
-module.exports = (testFile) => testFile.replace(regex, 'build/$<dir>$<dpath>$<fpath>.js');
